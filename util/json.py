b6bcb8b19642b2daa143a7af405737a0293522a7
Amyl11 <vumanhcvp@gmail.com>
2025-08-13 11:32:17 +0700

    Update thuộc tính tài khoản online

--- util/json.py
+++ util/json.py
@@ -11,3 +11,18 @@ def load_accounts_from_json(file_path):
     except Exception as e:
         print(f"Lỗi khi đọc file JSON: {e}")
         return []
+    
+def update_current_account(json_path, device_id, account_name):
+    """Cập nhật current_account cho thiết bị trong file JSON"""
+    with open(json_path, "r", encoding="utf-8") as f:
+        devices = json.load(f)
+
+    for dev in devices:
+        if dev["device"] == device_id:
+            dev["current_account"] = account_name
+            break
+
+    with open(json_path, "w", encoding="utf-8") as f:
+        json.dump(devices, f, indent=4, ensure_ascii=False)
+
+    print(f"Đã cập nhật current_account cho {device_id} → {account_name}")
