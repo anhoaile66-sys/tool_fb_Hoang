a1708f03de1b3094fd3f28327f48ed63b7492411
Amyl11 <vumanhcvp@gmail.com>
2025-08-18 16:06:21 +0700

    Thêm phần clear app sau khi chạy xong

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ DEVICES_LIST = [
     "7DXCUKKB6DVWDAQO"
     ]
 
+# Thoát app, xóa cache khi chờ task
+async def clear_app(driver):
+    driver.press("recent")
+    await asyncio.sleep(2)
+
+    size = driver.window_size()
+    width, height = size[0], size[1]
+
+    start_x = width / 2
+    end_x = start_x
+    start_y = height * 0.7
+    end_y = height * 0.2
+    duration = 0.04
+    sleep_time = 3
+
+    driver.swipe(start_x, start_y, end_x, end_y, duration=duration)
+    await asyncio.sleep(sleep_time)
+
 async def run_on_device(device_id):
     try:
         device = load_device_account(device_id)
@@ -23,13 +41,14 @@ async def run_on_device(device_id):
 
         other_account = [acc for acc in device['accounts'] if acc['name'] != device['current_account']]
         account = random.choice(other_account)
-
         log_message(f"Đang đăng nhập vào tài khoản {account['name']} trên thiết bị {device_id}")
         await swap_account(driver, account)
         device['current_account'] = account['name']
         update_current_account(device_id, account['name'])
         # tasks nuôi fb
         await fb_natural_task(driver)
+        # clear
+        await clear_app(driver)
     except Exception as e:
         log_message(f"Lỗi trên thiết bị {device_id}: {e}", logging.ERROR)
 
