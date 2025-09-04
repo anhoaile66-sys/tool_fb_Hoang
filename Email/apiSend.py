617761f4dd6e762c48db07556a43c663aadd6788
Vũ <149512389+bluff-king@users.noreply.github.com>
2025-09-04 16:20:23 +0700

    thêm docstring

--- Email/apiSend.py
+++ Email/apiSend.py
@@ -1,5 +1,6 @@
 from classSend import run_sent
 import time
+'''File này chỉ đụng đến nếu watchdog chiếm quá nhiều CPU'''
 
 # --- Config động ---
 EMP_ID = 22789191
@@ -15,5 +16,5 @@ SUBJECT = "Đây là tin nhắn test. Cơ hội việc làm IT dành cho bạn"
 
 while True:
     run_sent(EMP_ID, SUBJECT, CONTENT)
-    print("⏳ Chờ 3p  trước khi chạy  tiếp theo...")
-    time.sleep(30)
+    print("⏳ Chờ 1p  trước khi chạy  tiếp theo...")
+    time.sleep(60)
