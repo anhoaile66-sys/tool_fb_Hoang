90396022eec97625802a23f467f986f6587ccfc7
Amyl11 <vumanhcvp@gmail.com>
2025-09-04 15:12:47 +0700

    sửa lỗi không chạy fb

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import uiautomator2 as u2
 from module import *
 driver = u2.connect("EY5H9DJNIVNFH6OR")
-driver.app_start("com.facebook.katana")
+driver.app_start("com.zing.zalo")
 
 
-asyncio.run(add_friend(driver, '22615833'))
+# asyncio.run(add_friend(driver, '22615833'))
