eee49428fb66fbfd2d55c14afb15b8808a66a3f5
levanloc524hhp-oss <levanloc524.hhp@gmail.com>
2025-09-18 14:58:28 +0700

    test new git

--- main.py
+++ main.py
@@ -11,13 +11,8 @@ from typing import Optional, Callable
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 from id import DEVICE_LIST
+from util.log import log_message
 
-# ====== LOG UTIL (uses util.log if present) ======
-try:
-    from util.log import log_message
-except Exception:
-    def log_message(msg: str, level=logging.INFO):
-        print(msg)
 
 # ======================= CẤU HÌNH =======================
 HOME_PACKAGES = {
@@ -435,6 +430,8 @@ async def run_all_devices():
     # Khởi động supervisor cho mỗi device
     tasks = [asyncio.create_task(device_supervisor(did)) for did in DEVICE_LIST]
     await asyncio.gather(*tasks)
+
+
 if __name__ == "__main__":
     try:
         asyncio.run(run_all_devices())
